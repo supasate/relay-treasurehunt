b3bd3baa15bbd822bbe1abd41de707f411004d1f
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-14 21:50:35 +0700

    Create types and connection

--- data/schema.js
+++ data/schema.js
@@ -46,16 +46,81 @@ import {
  * The first method defines the way we resolve an ID to its object.
  * The second defines the way we resolve an object to its GraphQL type.
  */
+const { nodeInterface, nodeField } = nodeDefinitions(
+  (globalId) => {
+    const { type, id } = fromGlobalId(globalId)
 
+    if (type === 'Game') {
+      return getGame(id)
+    } else if (type === 'HidingSpot') {
+      return getHidingSpot(id)
+    } else {
+      return null
+    }
+  },
+  (obj) => {
+    if (obj instanceof Game) {
+      return gameType
+    } else if (obj instanceof HidingSpot) {
+      return hidingSpotType
+    } else {
+      return null
+    }
+  }
+)
 
 /**
  * Define your own types here
  */
+const gameType = new GraphQLObjectType({
+  name: 'Game',
+  description: ' A tresure search game',
+  fields: () => ({
+    id: globalIdField('Game'),
+    hidingSpots: {
+      type: hidingSpotConnection,
+      description: 'Places where treasure might be hidden',
+      args: connectionArgs,
+      resolve: (game, args) => connectionFromArray(getHidingSpots(), args),
+    },
+    turnsRemaining: {
+      type: GraphQLInt,
+      description: 'The number of turns a player has left to find the the treasure',
+    }
+  }),
+  interfaces: [nodeInterface],
+})
+
+const hidingSpotType = new GraphQLObjectType({
+  name: 'HidingSpot',
+  description: 'A place where you might find treasure',
+  fields: () => ({
+    id: globalIdField('HidingSpot'),
+    isChecked: {
+      type: GraphQLBoolean,
+      description: 'True if this hiding spot is checked for treasure',
+      resolve: (hidingSpot) => hidingSpot.isChecked,
+    },
+    hasTreasure: {
+      type: GraphQLBoolean,
+      description: 'True if this hiding spot holds treasure',
+      resolve: (hidingSpot) => {
+        if (hidingSpot.isChecked) {
+          return hidingSpot.hasTreasure
+        } else {
+          return null
+        }
+    }
+  }),
+  interfaces: [nodeInterface],
+})
 
 
 /**
  * Define your own connection types here
  */
+const { connectionType: hidingSpotConnection } =
+  connectionDefinitions({ name: 'HidingSpot', nodeType: hidingSpotType })
 
 /**
  * This is the type that will be the root of our query,
